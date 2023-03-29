b4a83b3c3e536f395e354d2ed90c48838b40e227
Ange Kabrel <ngoumkwa.kenne@gmail.com>
2023-03-29 04:55:51 +0100

    message

---
+++ database/migrations/2023_03_29_035035_create_produits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('npro')->primary();
+            $table->string('libelle');
+            $table->float('prix', 12, 3);
+            $table->float('qstock', 12, 3);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
